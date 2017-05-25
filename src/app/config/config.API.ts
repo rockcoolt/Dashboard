8ed27e948eb732debc98e6ad46fe8f714d99c12e
rockcoolt <rockcoolt@gmail.com>
2017-05-26 00:55:29 +0200

    up

--- src/app/config/config.API.ts
+++ src/app/config/config.API.ts
@@ -1,5 +1,5 @@
 export const API = {
-    // server: 'https://rockcoolt.xyz',
-    server: 'http://localhost:4000',
+    server: 'https://rockcoolt.xyz',
+    // server: 'http://localhost:4000',
     route: '/api/'
 }
